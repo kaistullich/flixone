7a1ff2b87a45c23bb8df7822b6662a5b544b9002
Kai Stullich <kairstullich@gmail.com>
2018-01-24 00:27:36 -0800

    addded env variables for storing API KEY and API BASE URL

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ const Index = (props) => (
 	<Layout>
 		{props.movieDetails.map((movie) => (
 			<Card
+				key={movie.id}
 				backdrop_path={movie.backdrop_path}
 				title={movie.title}
 				overview={movie.overview}
@@ -15,7 +16,7 @@ const Index = (props) => (
 )
 
 Index.getInitialProps = async function() {
-	const req = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=1421255c73535e975df165585fff44c7&language=en-US&query=home%20alone&page=1`)
+	const req = await fetch(`${process.env.API_BASE_URL}/search/movie?api_key=${process.env.API_KEY}&language=en-US&query=home%20alone&page=1`)
 	const data = await req.json()
 	/*
 	The `data` variable contains an array with dictionaries
