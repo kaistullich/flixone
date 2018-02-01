27f565882831ad649548665c6736ac1bd6c2e129
Kai Stullich <kairstullich@gmail.com>
2018-01-31 19:10:37 -0800

    removed bootstrap and added Materialize CSS

--- components/Meta/bs.theme.js
+++ components/Meta/bs.theme.js
@@ -1,11 +1,11 @@
 import Head from "next/head";
 
 const Theme = (props) => (
-	<div>
+	<div className='container'>
 		<Head>
 			<meta charSet='utf-8' />
 			<meta name='viewport' content='initial-scale=1.0, width=device-width' />
-			<link rel='stylesheet' href='../../static/bootstrap.min.css' />
+			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css" />
 		</Head>
 		{props.children}
 	</div>
